e17bf8d573907d331dc59d4d3707688a060b4d11
Sam Hall <mann.samantha11@gmail.com>
2020-12-01 20:47:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,9 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), organized_array|
-    binding.pry
+    #binding.pry
+    value.each do |inner_key, name|
+      binding.pry
   end
   organized_array
 binding.pry
