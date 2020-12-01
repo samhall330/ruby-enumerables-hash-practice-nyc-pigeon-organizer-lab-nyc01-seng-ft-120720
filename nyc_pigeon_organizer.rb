5ff937155ae4271f2befd52365fad8666fc80779
Sam Hall <mann.samantha11@gmail.com>
2020-12-01 21:07:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,14 +2,14 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   organized_data = data.each_with_object({}) do |(key, value), organized_array|
-  
+
     value.each do |inner_key, names|
-    
+
       names.each do |name|
-      
+
         if !organized_array[name]
           organized_array[name] = {}
-        
+
         end
         if !organized_array[name][key]
           organized_array[name][key] =[]
